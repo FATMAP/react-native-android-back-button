166a99b60a00014cde89ca708488b2d7fc4361d8
Oli Barnett <oli.barnett@gmail.com>
2018-01-17 16:41:05 +0100

    fix using BackAndroid instead of BackHandler

--- src/index.js
+++ src/index.js
@@ -1,56 +1,54 @@
-import React, { Component } from "react"
-import PropTypes from 'prop-types'
-import { AppState, BackHandler, Platform } from "react-native"
-import withSideEffect from "react-side-effect"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { AppState, BackHandler, Platform } from "react-native";
+import withSideEffect from "react-side-effect";
 
-var listener = null
-var backButtonPressFunction = () => false
+var listener = null;
+var backButtonPressFunction = () => false;
 
 class AndroidBackButton extends Component {
   componentDidMount() {
     if (Platform.OS === "android") {
-      AppState.addEventListener('change', state => {
-        if (state == 'background') {
+      AppState.addEventListener("change", state => {
+        if (state == "background") {
           listener = null;
         }
-      })
+      });
     }
 
     if (Platform.OS === "android" && listener === null) {
       listener = BackHandler.addEventListener("hardwareBackPress", () => {
-        return backButtonPressFunction()
-      })
+        return backButtonPressFunction();
+      });
     }
   }
 
   componentWillUnmount() {
     if (Platform.OS === "android" && listener !== null) {
-      BackAndroid.removeEventListener('hardwareBackPress', listener);
+      BackHandler.removeEventListener("hardwareBackPress", listener);
     }
   }
 
   render() {
-    return null
+    return null;
   }
 }
 
 AndroidBackButton.propTypes = {
   onPress: PropTypes.func.isRequired
-}
+};
 
 function reducePropsToState(propsList) {
-  const defaultValue = () => false
-  const lastComponent = propsList[propsList.length - 1]
-  return (lastComponent && lastComponent.onPress) || defaultValue
+  const defaultValue = () => false;
+  const lastComponent = propsList[propsList.length - 1];
+  return (lastComponent && lastComponent.onPress) || defaultValue;
 }
 
 function mapStateOnServer(callback) {
-  backButtonPressFunction = callback
-  return backButtonPressFunction
+  backButtonPressFunction = callback;
+  return backButtonPressFunction;
 }
 
-export default withSideEffect(
-  reducePropsToState,
-  () => {},
-  mapStateOnServer
-)(AndroidBackButton)
+export default withSideEffect(reducePropsToState, () => {}, mapStateOnServer)(
+  AndroidBackButton
+);
